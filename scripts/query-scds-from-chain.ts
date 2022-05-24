e81fef9c1648f6ab71451b03bb825d9aa3eb57f0
GarondEisenfaust <chr.schr@web.de>
2022-05-24 16:21:53 +0200

    query script takes the query now from an env variable

--- scripts/query-scds-from-chain.ts
+++ scripts/query-scds-from-chain.ts
@@ -10,14 +10,21 @@ const walletAddress = process.env.WALLET_ADDRESS
   ? process.env.WALLET_ADDRESS
   : "0x250548444A4fBcFfb273B1034aa32cD6828544b0";
 
+const query = process.env.QUERY;
+
 async function main() {
+  if (!query) {
+    console.error("You have to specify a query!");
+    return;
+  }
+
   const provider = new ethers.providers.JsonRpcProvider("http://localhost:8545");
   const signer = provider.getSigner(walletAddress);
   const registry = Registry__factory.connect(registryAddress, signer);
 
   console.log("started");
   const start = performance.now();
-  const result = await registry.query("Name='Contract'");
+  const result = await registry.query(query);
   const end = performance.now();
   result.forEach(metadata => console.log(`id: ${metadata.id} name: ${metadata.metadata.name}`));
   console.log(`Retrieval took ${end - start} ms`);
