5b57f3fb15f66fcdb5c627da95c7ff455a0b31ac
GarondEisenfaust <chr.schr@web.de>
2022-05-24 14:32:03 +0200

    makes more parameters of the Stress test changeable with env variables

--- stress-test/StressTest.ts
+++ stress-test/StressTest.ts
@@ -9,7 +9,7 @@ import { readFileSync } from "fs";
 import { toContractType } from "../external/decentralised-scd-registry-common/src/Conversion";
 import _ from "lodash";
 import { Registry__factory } from "src/types";
-import * as RegistryDeployment from "../deployments/ganache-cli/Registry.json";
+import "dotenv/config";
 
 function scdToMetadata(scd: SCD, url: string): Metadata {
   const functionNames = scd.functions ? scd.functions.map(func => func.name).filter(name => name) : [];
@@ -32,12 +32,20 @@ function scdToMetadata(scd: SCD, url: string): Metadata {
 }
 
 const scdDir = process.env.SCD_DIR ? process.env.SCD_DIR : join(ProjectRootDir.get(), "stress-test-scds");
+const hostIp = process.env.HOST_IP ? process.env.HOST_IP : "localhost";
+const registryAddress = process.env.REGISTRY_ADDRESS
+  ? process.env.REGISTRY_ADDRESS
+  : "0x222E34DA1926A9041ed5A87f71580D4D27f84fD3";
+const walletAddress = process.env.WALLET_ADDRESS
+  ? process.env.WALLET_ADDRESS
+  : "0x250548444A4fBcFfb273B1034aa32cD6828544b0";
+
 const storeScds = process.env.STORE == "true";
 
 async function prepare(): Promise<Registry> {
   const provider = new ethers.providers.JsonRpcProvider("http://localhost:8545");
-  const signer = provider.getSigner("0x250548444A4fBcFfb273B1034aa32cD6828544b0");
-  const registry = Registry__factory.connect(RegistryDeployment.address, signer);
+  const signer = provider.getSigner(walletAddress);
+  const registry = Registry__factory.connect(registryAddress, signer);
 
   if (storeScds) {
     const scdMetadataGroups = _.chunk(
@@ -46,9 +54,7 @@ async function prepare(): Promise<Registry> {
         .filter(scdPath => scdPath.endsWith(".json"))
         .map(scdPath => {
           const scdData = JSON.parse(readFileSync(scdPath, "utf-8"));
-          return toContractType(
-            scdToMetadata(scdData, join("http://192.168.178.22:49160/", relative(scdDir, scdPath))),
-          );
+          return toContractType(scdToMetadata(scdData, join(`http://${hostIp}:49160/`, relative(scdDir, scdPath))));
         }),
       500,
     );
